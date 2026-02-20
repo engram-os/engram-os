efa09f751022a38b1f171c230934ad9eacb57f45
Varun Salian <varuntheone130@gmail.com>
2026-02-20 11:18:33 +0530

    fix: remove unused argument from CORSMiddleware config

--- core/brain.py
+++ core/brain.py
@@ -37,10 +37,9 @@ app = FastAPI(title="Engram OS Brain")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
-    allow_credentials=True,
-    allow_methods=["*"],  
-    allow_headers=["*"],  
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(terminal_router)
